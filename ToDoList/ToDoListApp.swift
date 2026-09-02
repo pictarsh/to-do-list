06e773bbd8cc721ce61de6f5f411456ebaaef89a
Coding Bee Academy <rian.lesmana@codingbee.org>
2026-09-02 12:02:05 +0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by mac on 02/09/26.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
